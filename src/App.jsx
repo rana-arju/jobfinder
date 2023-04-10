83241d4364e9579f5f46af132ded1cda70f19270
rana-arju <arjurana20@gmail.com>
2023-04-10 10:04:18 -0700

    category section design complate with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,49 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Hero from "./components/Hero/Hero";
 import Footer from "./components/footer/Footer";
-
+import icon1 from "./assets/Icons/accounts 1.png";
+import icon2 from "./assets/Icons/business 1.png";
+import icon3 from "./assets/Icons/social-media 1.png";
+import icon4 from "./assets/Icons/chip 1.png";
+import Category from "./components/category/Category";
 function App() {
+  const category = [
+    {
+      icon: icon1,
+      title: "Account & Finance",
+      des: "300 Jobs Available",
+    },
+    {
+      icon: icon2,
+      title: "Creative Design",
+      des: "100+ Jobs Available",
+    },
+    {
+      icon: icon3,
+      title: "Marketing & Sales",
+      des: "150 Jobs Available",
+    },
+    {
+      icon: icon4,
+      title: "Engineering Job",
+      des: "224 Jobs Available",
+    },
+  ];
   return (
     <>
       <Header />
       <Hero />
+      <div className="my-20 w-10/12 mx-auto">
+        <div className="text-center">
+          <h2 className="text-3xl font-bold">Job Category List</h2>
+          <p className="text-gray-500">
+            Explore thousands of job opportunities with all the information you
+            need. Its your future
+          </p>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10 mt-10">{category.map((cat,i) => <Category key={i} cat={cat} />)}</div>
+      </div>
+
       <Footer />
     </>
   );
---
+++ src/components/category/Category.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Category = ({ cat }) => {
+  console.log(cat);
+  return (
+    <div className="card card-compact bg-[#FAF8FF]  py-8">
+      <figure>
+        <img src={cat.icon} alt={cat.title} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{cat.title}</h2>
+        <p>{cat.des}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
